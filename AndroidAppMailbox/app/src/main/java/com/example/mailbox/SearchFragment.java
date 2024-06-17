9fabd21cc3bec5ba47c0b4476b4daa008ad18bce
Maximo309 <com.company.inc@gmail.com>
2024-06-17 20:15:23 +0300

    17/06/24 20-15

--- AndroidAppMailbox/app/src/main/java/com/example/mailbox/SearchFragment.java
+++ AndroidAppMailbox/app/src/main/java/com/example/mailbox/SearchFragment.java
@@ -10,24 +10,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SearchFragment extends Fragment {
-    //String arg;
+    EditText searchLine;
+    Button searchButton;
+    ListView surchList;
+    ArrayAdapter<String> adapter;
 
-    //public SearchFragment (String arg)
-    //{
-        //this.arg = arg;
-    //}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        //Log.d("SearchFragment", "onCreateView() called");
-        //View view = getView();
-        ////TextView textView = view.findViewById(R.id.text_view);
-        //EditText searchLine = view.findViewById(R.id.searchLine);
-        //searchLine.setText("qwe");
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
@@ -37,7 +34,39 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        EditText searchLine = view.findViewById(R.id.searchLine);
-        searchLine.setText("qwe");
+        searchLine = view.findViewById(R.id.searchLine);
+
+        surchList = view.findViewById(R.id.surchList);
+
+        searchButton = view.findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inpData(searchLine.getText().toString());
+            }
+        });
+    }
+
+    public void inpData(String addressMail)
+    {
+        Client client = new Client("SEARCHPEOPLE>>"+addressMail);
+        Thread thread = new Thread(client);
+        thread.start();
+
+        String answer = null;
+        synchronized (thread) {
+            try {
+                thread.join(); // Ожидаем завершения работы потока
+
+                answer = client.response;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String[] res = answer.split("<<");
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, res);
+        surchList.setAdapter(adapter);
     }
 }
