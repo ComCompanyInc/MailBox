a95b8cbb446edaf07ed0237dffec706a97eb5a55
Maximo309 <com.company.inc@gmail.com>
2024-06-17 16:46:33 +0300

    17/06/24 16-45 (solvedTransactionFromFragments)

--- AndroidAppMailbox/app/src/main/java/com/example/mailbox/SearchFragment.java
+++ AndroidAppMailbox/app/src/main/java/com/example/mailbox/SearchFragment.java
@@ -2,18 +2,42 @@ package com.example.mailbox;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class SearchFragment extends Fragment {
+    //String arg;
 
+    //public SearchFragment (String arg)
+    //{
+        //this.arg = arg;
+    //}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Log.d("SearchFragment", "onCreateView() called");
+        //View view = getView();
+        ////TextView textView = view.findViewById(R.id.text_view);
+        //EditText searchLine = view.findViewById(R.id.searchLine);
+        //searchLine.setText("qwe");
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText searchLine = view.findViewById(R.id.searchLine);
+        searchLine.setText("qwe");
     }
 }
